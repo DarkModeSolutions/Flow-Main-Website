14d5469c44d640dae681d8420046c96bc6498ca0
pranulkbv28 <28bvpranaav@gmail.com>
2025-10-09 01:07:02 +0530

    feat(types): add type declarations for image formats and react-icons

--- flow-main-website/src/types/globals.d.ts
+++ flow-main-website/src/types/globals.d.ts
@@ -7,3 +7,26 @@ declare module "*.scss" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "react-icons/*";
+
+declare module "*.png" {
+  import { StaticImageData } from "next/image";
+  const value: StaticImageData;
+  export default value;
+}
+declare module "*.jpg" {
+  import { StaticImageData } from "next/image";
+  const value: StaticImageData;
+  export default value;
+}
+declare module "*.jpeg" {
+  import { StaticImageData } from "next/image";
+  const value: StaticImageData;
+  export default value;
+}
+declare module "*.svg" {
+  import { StaticImageData } from "next/image";
+  const value: StaticImageData;
+  export default value;
+}
