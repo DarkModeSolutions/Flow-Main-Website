199d9f0cebc9fc1b81faddab5ef0091a1a6279df
pranulkbv28 <28bvpranaav@gmail.com>
2025-11-24 20:42:10 +0530

    Revert "chore: update prisma dependencies and configuration"
    This reverts commit b48fcaac02c7d45aa95857e79e75de8b85eb9753.

--- flow-main-website/src/components/ErrorComponent.tsx
+++ flow-main-website/src/components/ErrorComponent.tsx
@@ -1,23 +1,12 @@
-import Link from "next/link";
+import React from "react";
 
-const ErrorComponent = ({
-  error,
-  redirectToLogin,
-}: {
-  error: unknown;
-  redirectToLogin?: boolean;
-}) => {
+const ErrorComponent = ({ error }: { error: unknown }) => {
   return (
     <div className="w-full flex items-center justify-center min-h-screen">
       <div className="text-center">
         <h2 className="text-2xl font-bold text-red-600 mb-2">Error</h2>
         <p className="text-gray-600">{`${error}`}</p>
       </div>
-      {redirectToLogin && (
-        <Link href={"/auth/login"} className="mt-4 text-blue-500 underline">
-          Please login to continue.
-        </Link>
-      )}
     </div>
   );
 };
